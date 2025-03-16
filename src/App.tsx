bf8a918ec2aec5ca09513f6b71c6a5afc79b878d
warathepj <80021220+warathepj@users.noreply.github.com>
2025-03-16 11:01:37 +0700

    test ok

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MqttMessages } from "@/components/MqttMessages";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
@@ -13,6 +14,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      <MqttMessages />
       <Toaster />
       <Sonner />
       <BrowserRouter>
